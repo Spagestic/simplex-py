fcd9fbab8b0ebbac2a481f74f790f1ce9e0dee96
Spagestic <steelmax666@gmail.com>
2025-02-08 21:53:31 +0800

    add logger configuration utility for improved logging setup

---
+++ utils/logger_config.py
@@ -0,0 +1,13 @@
+import logging
+
+def setup_logger(name, log_file, level=logging.INFO):
+    """Sets up a logger with the specified name, log file, and logging level."""
+    handler = logging.FileHandler(log_file)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+
+    return logger
