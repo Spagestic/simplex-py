9b81b3e2742fb397b4922d4515060a64d9b47155
Spagestic <steelmax666@gmail.com>
2025-02-09 09:45:33 +0800

    add new example problems for unbounded maximization, infeasible minimization, and maximization with equality

--- webapp/example_problems.py
+++ webapp/example_problems.py
@@ -20,5 +20,26 @@ example_problems = {
         "rhs_values": [4, 6],
         "senses": ['<=', '>='],
         "problem_type": "max"
+    },
+    "Example 4: Unbounded Maximization": {
+        "objective_coeffs": [1, 1],
+        "constraint_matrix": [[1, -1], [-1, 1]],
+        "rhs_values": [1, 1],
+        "senses": ['>=', '>='],
+        "problem_type": "max"
+    },
+    "Example 5: Infeasible Minimization": {
+        "objective_coeffs": [3, 2],
+        "constraint_matrix": [[1, 1], [2, 3]],
+        "rhs_values": [5, 15],
+        "senses": ['<=', '<='],
+        "problem_type": "min"
+    },
+    "Example 6: Maximization with Equality": {
+        "objective_coeffs": [4, 3],
+        "constraint_matrix": [[1, 1], [2, 1]],
+        "rhs_values": [3, 4],
+        "senses": ['=', '<='],
+        "problem_type": "max"
     }
 }
