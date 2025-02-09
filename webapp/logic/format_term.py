1a2471f326f1d34800322b5b1e8ab540934500fe
Spagestic <steelmax666@gmail.com>
2025-02-09 08:57:15 +0800

    refactor format_term function to enhance term formatting and support integer coefficients

--- webapp/logic/format_term.py
+++ webapp/logic/format_term.py
@@ -1,14 +1,32 @@
-def format_term(coeff, index):
-    """Format a term in the objective function or constraint."""
-    if coeff == 0:
+def format_term(coefficient, index, is_integer=False):
+    """
+    Formats a single term in the linear programming problem.
+
+    Args:
+        coefficient (float): The coefficient of the term.
+        index (int): The index of the variable (starting from 0).
+        is_integer (bool): True if the coefficient is an integer, False otherwise.
+
+    Returns:
+        str: The formatted term as a string.
+    """
+    if coefficient == 0:
         return ""
-    term = ""
-    if coeff > 0:
-        term += "+ "
+
+    if is_integer:
+        coeff_str = str(int(coefficient))
     else:
-        term += "- "
-        coeff = abs(coeff)  # Use absolute value for negative coefficients
-    if coeff != 1:
-        term += f"{coeff}"
-    term += f"x_{index+1}"
+        coeff_str = f"{coefficient:.2f}"
+
+    if coefficient > 0:
+        sign = "+"
+    else:
+        sign = "-"
+        coeff_str = coeff_str[1:]  # Remove the negative sign
+
+    if coeff_str == "1":
+        term = f"{sign} x_{index + 1}"
+    else:
+        term = f"{sign} {coeff_str}x_{index + 1}"
+
     return term
